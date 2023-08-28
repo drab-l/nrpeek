ca28b6ee3bfcb8be9eaf9078f527e33df7c1aa84
drab <23292996+drab-l@users.noreply.github.com>
2023-08-28 20:30:08 +0900

    declarate pid as integer in windows

--- src/windows.rs
+++ src/windows.rs
@@ -1,24 +1,25 @@
 //! Wrapper for readvm C function
 use std::io::{Result, Error};
 use std::ffi::*;
-pub type Pid = *const std::ffi::c_void;
+pub type Pid = usize;
+type HANDLE = *const std::ffi::c_void;
 
 mod c {
     use std::ffi::*;
-    use super::Pid;
+    use super::*;
     extern "system" {
-        pub fn GetCurrentProcess() -> Pid;
-        pub fn ReadProcessMemory(pid: Pid, src: *const c_void, dst: *mut c_void, size: usize, rsize: *mut usize) -> c_int;
+        pub fn GetCurrentProcess() -> HANDLE;
+        pub fn ReadProcessMemory(pid: HANDLE, src: *const c_void, dst: *mut c_void, size: usize, rsize: *mut usize) -> c_int;
     }
 }
 
 pub fn getpid() -> Pid {
-    unsafe { c::GetCurrentProcess() }
+    (unsafe { c::GetCurrentProcess() }) as usize
 }
 
 pub unsafe fn peek_buf(pid: Pid, addr: usize, dst: *mut u8, size: usize) -> Result<usize> {
     let mut rsize = 0;
-    match c::ReadProcessMemory(pid, addr as *const c_void, dst.cast::<c_void>(), size, &mut rsize) {
+    match c::ReadProcessMemory(pid as HANDLE, addr as *const c_void, dst.cast::<c_void>(), size, &mut rsize) {
         0 => Err(Error::last_os_error()),
         _ => Ok(rsize),
     }
